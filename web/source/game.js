9f01dcf9264b7bae85b8b9a456610ea7cb1d08bb
davibelini <dev.dado.belini@gmail.com>
2021-03-14 16:00:26 -0300

    created canvas

--- web/source/game.js
+++ web/source/game.js
@@ -1,5 +1,18 @@
-class Game {
-  init() {
-    alert("hello world");
-  };
-};
+const canvas = document.getElementById("c")
+
+CANVAS_SIZE = [
+  window.outerHeight + 1000,
+  window.outerWidth + 1000
+]
+
+canvas.height = CANVAS_SIZE[0]
+canvas.width  = CANVAS_SIZE[1]
+
+///////////////////////////////////////////
+
+const ctx = canvas.getContext("2d")
+
+ctx.fillStyle = "black"
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+window.scroll(50, 50)
